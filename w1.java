7953c4378026c0c99a09e37fafc9f1c726dfab79
landy715 <landy715@gmail.com>
2014-02-24 03:05:20 +0800

    committed w1.java

---
+++ w1.java
@@ -0,0 +1,8 @@
+public class w1 {
+    public static void main(String[] args) { 
+        System.out.println("多情卻似總無情");
+		System.out.println("唯覺樽前笑不成");
+		System.out.println("蠟燭有心還惜別");
+		System.out.println("替人垂淚到天明");
+    }
+}
